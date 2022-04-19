5f347a2c7289d82bbc979430f4d6227e73495971
Fernando Zalazar <f.zalazar@live.com>
2022-04-19 11:19:26 -0300

    agego html

---
+++ algoritmo.js
@@ -0,0 +1,18 @@
+/*
+martes 19/4/2022
+acciones primitivas
+*/
+var palabra = "texto";
+ var numero = 2;
+ var decimal =3.4;
+ var boleano = true;
+
+ 
+var respuesta = prompt("ingrese su nombre",["nombre"]);
+console.log(respuesta);
+document.write(`
+   <h4>Nombre: </4>
+   <h4>${ numero } </4>
+
+`)
+ 
